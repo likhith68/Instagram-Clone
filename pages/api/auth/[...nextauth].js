81a5645269a2f19cd9ed02a3be6bccbc580b3255
Likhith68 <Likhithjsc@gmail.com>
2022-06-07 19:36:51 +0530

    ''

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
         // ...add more providers here
     ],
